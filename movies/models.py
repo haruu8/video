ae05b3fdb30458a84675540cdcf9331ad8a24dab
haru <real.hqru@gmail.com>
2020-09-02 13:08:08 +0900

    add admin authority

--- movies/models.py
+++ movies/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class MovieContent(models.Model):
+
+    class Meta(object):
+        db_table = 'movie'
+
+    title = models.CharField(max_length=255, null=False)
+    discription = models.CharField(max_length=355, null=True)
+    movie = models.FileField()
